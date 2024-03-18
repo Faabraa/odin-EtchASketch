cd053880c69d68467ed3438ad43280857f443fb7
Faabraa <fabramontt@gmail.com>
2024-03-18 19:00:02 +0100

    mouseDrawing created - Now if you pass through the container the div will be painted with black

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
+const container = document.querySelector(".container");
+let isDrawing = false;
 createDivs();
-function createDivs()   {
-    const container = document.querySelector(".container");
-    for(let i = 0; i < 16*16; i++) {
+mouseDrawing();
+resetDraw();
+/**
+ * Creates a 16 * 16 where you will be able to draw with your cursor
+ */
+function createDivs() {
+    for (let i = 0; i < 16 * 16; i++) {
         let div = document.createElement("div");
-        div.textContent = (i+1);
         div.className = "square";
         container.appendChild(div);
     }
+}
+/**
+ * Detects where is the mouse, and if inside the container (On top of the divs)
+ * Lets you draw, changing the background color to black
+ */
+function mouseDrawing() {
+    //Reads mouse inside container
+    container.addEventListener('mouseenter', () => {
+        isDrawing = true;
+    });
+    //Reads mouse outside container
+    container.addEventListener('mouseleave', () =>  {
+        isDrawing = false;
+    });
+    container.addEventListener('mousemove', (event) => {
+        if (isDrawing) {
+            //If the cursor is on top of the square
+            if (event.target.classList.contains('square')) {
+                //Draw in black (each div)
+                event.target.style.backgroundColor = 'black';
+            }
+        }
+    });
+}
+/**
+ * If you press a button the draw will be restarted
+ */
+function resetDraw() {
+
 }
